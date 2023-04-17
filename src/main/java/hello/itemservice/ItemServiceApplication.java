7b6f2763dab01257564a12a11b9a7fdabcb8f303
WanDuKong-pea <sophisticateo_o@naver.com>
2023-04-17 17:27:50 +0900

    Spring MVC2 Validation1_Validator분리2_(WebDataBinder 사용_@InitBinder_@Validated)

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,7 +1,9 @@
 package hello.itemservice;
 
+import hello.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +11,10 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
+
+	//WebDataBinder 글로벌 설정 시 사용. (잘 안씀_BeanValidator가 자동 등록되지 않음)
+	//컨트롤러에서 @initBinder를 사용하지 않아도 정상 작동
+	//implements WebMvcConfigurer
+	//@Override public Validator getValidator() { return new ItemValidator();}
 
 }
--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -240,6 +242,7 @@ public class ValidationItemControllerV2 {
     }
     */
 
+    /*
     @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes){
@@ -263,6 +266,46 @@ public class ValidationItemControllerV2 {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "validation/v2/editForm";
+    }
+    */
+
+    /**
+     * WebDataBinder에 검증기를 추가하면 해당 컨트롤러에서는
+     * 검증기를 자동으로 적용할 수 있음
+     * @initBinder: 해당 컨트롤러에만 영향
+     * 글로벌은 별도 설정 필요
+     * @param dataBinder
+     */
+    @InitBinder
+    public void init(WebDataBinder dataBinder){
+        log.info("init binder {}", dataBinder);
+        dataBinder.addValidators(itemValidator);
+        //아래와 같이 여러개 validator를 등록 가능
+        //validator의 support()메서드로 검증 대상을 구분함
+        //dataBinder.addValidators(userValidator);
+        //dataBinder.addValidators(xxxValidator);
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes){
+        //파라미터에 @Validated 추가됨 <- 검증기를 실행하라는 애노테이션
+        //겁증 대상(item) 앞에 붙임
+        //@Validated 애노테이션이 붙으면
+        //앞서 WebDataBinder에 등록한 검증기를 찾아 실행후 BindingResult 에 검증 결과를 담음
+
+        //이전 버전 코드 생략 됨.
+        //itemValidator.validate(item,bindingResult);
+
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+            return "validation/v2/addForm";
+        }
+        //성공로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status",true);
+        return "redirect:/validation/v2/{itemId}";
     }
 
     @PostMapping("/{itemId}/edit")
